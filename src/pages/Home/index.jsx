3110ca6de7676e6e59daf57b6f19432e029ee3fc
Marcelo-RM <marcelordrgs98@gmail.com>
2022-03-03 16:55:25 -0300

    Iniciado Carousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,70 @@
 import React, { useState } from "react";
-import { Container, Header } from "./styles";
+import {
+  Container, Header, Wrapper, Map, CarouseTitle
+} from "./styles";
 import logo from "../../assets/logo.svg"
-import { TextField, Input } from '@mui/material';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import SearchIcon from '@mui/icons-material/Search';
+
+import Slider from 'react-slick';
 
 const Home = function () {
 
   const [inputValue, setInputValue] = useState("");
 
-  return <Container>
-    <Header>
-      <img src={logo} alt="Logo - RestaurantFinder" width="50%" style={{ margin: 'auto' }} />
-      <TextField
-        variant="outlined"
-        label="Pesquisar..."
-        value={inputValue}
-        onChange={(e) => { setInputValue(e.target.value); }} />
-    </Header>
-  </Container>;
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    adaptiveHeight: true
+  };
+
+  return <Wrapper>
+    <Container>
+      <Header>
+        <img src={logo} alt="Logo - RestaurantFinder" width="40%" style={{ margin: 'auto' }} />
+
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Pesquisar</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            value={inputValue}
+            onChange={(e) => { setInputValue(e.target.value); }}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  edge="end"
+                >
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Pesquisar"
+          />
+        </FormControl>
+        <CarouseTitle>Sua área</CarouseTitle>
+        <Slider {...settings}>
+          <img alt="" src="https://youmustgo.com.br/wp-content/uploads/2019/09/restaurantes-na-rua-dias-ferreira-5.jpeg" />
+          <img alt="" src="https://youmustgo.com.br/wp-content/uploads/2019/09/restaurantes-na-rua-dias-ferreira-5.jpeg" />
+          <img alt="" src="https://youmustgo.com.br/wp-content/uploads/2019/09/restaurantes-na-rua-dias-ferreira-5.jpeg" />
+          <img alt="" src="https://youmustgo.com.br/wp-content/uploads/2019/09/restaurantes-na-rua-dias-ferreira-5.jpeg" />
+          <img alt="" src="https://youmustgo.com.br/wp-content/uploads/2019/09/restaurantes-na-rua-dias-ferreira-5.jpeg" />
+          <img alt="" src="https://youmustgo.com.br/wp-content/uploads/2019/09/restaurantes-na-rua-dias-ferreira-5.jpeg" />
+          <img alt="" src="https://youmustgo.com.br/wp-content/uploads/2019/09/restaurantes-na-rua-dias-ferreira-5.jpeg" />
+        </Slider>
+      </Header>
+    </Container>
+    <Map />
+  </Wrapper>;
 }
 
 export default Home;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
 export const Container = styled.aside`
   background-color: ${(props) => props.theme.colors.background};
   width: 360px;
@@ -12,4 +17,18 @@ export const Header = styled.section`
   flex-direction: column;
   background-color: #ffffff;
   padding: 16px;
+`;
+
+export const Map = styled.div`
+  background-color: purple;
+  width: 100px;
+`;
+
+export const CarouseTitle = styled.h1`
+  font-family: ${(props) => props.theme.fonts.regular};
+  color: ${(props) => props.theme.colors.text};
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 29px;
+  margin: 16px 0;
 `;
